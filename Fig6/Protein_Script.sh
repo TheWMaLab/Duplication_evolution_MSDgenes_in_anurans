9dab15af8594a1c223b0abf7cca6d36ff2c274a9
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2025-08-27 17:09:39 +0200

    Add files via upload

---
+++ Fig6/Protein_Script.sh
@@ -0,0 +1,11 @@
+#Convert Dmrt1 codon sequence to protein using guidance Prank
+perl $EBROOTGUIDANCE/www/Guidance/guidance.pl --program GUIDANCE --seqFile pelo_dup.fa  --msaProgram PRANK --seqType codon --outDir . --genCode 1 --bootstraps 100 --proc_num 32
+
+#reorder the names using output file
+awk '{print "s/\\b" $2 "\\b/" $1 "/g"}' Seqs.Codes > sed_script.sed
+sed -f sed_script.sed MSA.PRANK.PROT.aln > protein_align.txt
+sed '/^>/s/$/@/' protein_align.txt |sed -z 's/\n//g'  |sed 's/>/\n>/g' |sed 's/@/\n/g'  >Oneline_protein_aln.fa
+for i in `cat list` ;do grep -w -A1 "$i" Oneline_protein_aln.fa >>Reordered.protein.fa;done
+
+#Final reordered file 
+Reordered.protein.fa
